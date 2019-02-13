497df4cd299e7337c1a2ac930441a03906e75fd4
klkirstyleeskl <k.lees@btinternet.com>
2019-02-13 10:41:54 +0000

    passes getNumber test

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int number;
+    private String type;
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private double nightlyRate;
+
+    public Bedroom(int number, String type, int capacity, double nightlyRate){
+        this.number = number;
+        this.type = type;
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+        this.nightlyRate = nightlyRate;
+
+    }
+
+
+    public int getNumber(){
+        return this.number;
+    };
+
+
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class BedroomTest {
+    Bedroom bedroom;
+
+
+    @Before
+    public void before(){
+        bedroom = new Bedroom(17, "Suite", 4, 155.50);
+    }
+
+    @Test
+    public void hasNumber(){
+        assertEquals(17, bedroom.getNumber());
+    }
 }
