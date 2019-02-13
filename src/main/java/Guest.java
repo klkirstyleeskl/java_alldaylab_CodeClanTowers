26c2bfa6b7025c179b2d898fc59636ce1ce055f7
klkirstyleeskl <k.lees@btinternet.com>
2019-02-13 11:11:55 +0000

    passes GETTER tests for gueat class

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,2 +1,21 @@
+
+
 public class Guest {
+    private String guestName;
+    private double guestWallet;
+
+
+
+    public Guest(String guestName, double guestWallet){
+        this.guestName = guestName;
+        this.guestWallet = guestWallet;
+    }
+
+    public String getName(){
+        return guestName;
+    }
+
+    public double getWallet(){
+        return guestWallet;
+    }
 }
--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("Kirsty", 1000.01);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Kirsty", guest.getName());
+    }
+
+    @Test
+    public void hasMoneyInWallet(){
+        assertEquals(1000.01, guest.getWallet(), 0.01);
+    }
 }
